ebcd202323d761e57e832ae119642cdee48df89f
NikolaTesla17 <44534701+NikolaTesla17@users.noreply.github.com>
2020-06-25 23:14:16 -0700

    added all files

---
+++ script.js
@@ -0,0 +1,178 @@
+var nameArray = [];
+
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  }
+}
+
+$("#pick").click(function() {
+    // Get the input value
+  var names = document.getElementById("names").value;
+  
+  // Seperate the names and push them into the array
+  var nameArray = names.split(',');
+  
+  // Get a random name, the winner
+  //var winner = nameArray[Math.floor(Math.random()*nameArray.length)];
+  
+  shuffleArray(nameArray);
+  winner = nameArray[0];
+
+  var runnersUp = "";
+  for(var i = 1; i <= nameArray.length - 1; i++) {
+    runnersUp += i;
+    runnersUp += ".";
+    runnersUp += nameArray[i];
+    runnersUp += " ";
+  }
+
+  runnerUp = "Runners up: " + runnersUp;
+
+  //winner = "🎉" + " " + winner + " " + "🎉";
+  //winner = winner;
+
+  // Display winner
+  $("#world").addClass("open");
+  $("#winner").addClass("open");
+  $("#close").addClass("open");
+  $("#winner").text(winner);
+
+  $("#runnerUp").addClass("open");
+  $("#runnerUp").addClass("close");
+  $("#runnerUp").text(runnerUp);
+
+  // $("#world2").addClass("open");
+  // $("#names").addClass("open");
+  // $("#close2").addClass("open");
+  // $("#names").text(winner);
+  // $("#names").text(nameArray[1]);
+});
+  
+$("#close").click(function() {
+  $("#world").removeClass("open");
+  $("#winner").removeClass("open");
+  $("#runnerUp").removeClass("open");
+  $("#close").removeClass("open");
+
+});
+
+// Confetti
+(function() {
+  var COLORS, Confetti, NUM_CONFETTI, PI_2, canvas, confetti, context, drawCircle, i, range, resizeWindow, xpos;
+
+  NUM_CONFETTI = 350;
+
+  COLORS = [[0, 102, 204], [51, 153, 255], [0, 0, 153], [76, 0, 153], [0, 255, 255]];
+
+  PI_2 = 2 * Math.PI;
+
+  canvas = document.getElementById("world");
+
+  context = canvas.getContext("2d");
+
+  window.w = 0;
+
+  window.h = 0;
+
+  resizeWindow = function() {
+    window.w = canvas.width = window.innerWidth;
+    return window.h = canvas.height = window.innerHeight;
+  };
+
+  window.addEventListener('resize', resizeWindow, false);
+
+  window.onload = function() {
+    return setTimeout(resizeWindow, 0);
+  };
+
+  range = function(a, b) {
+    return (b - a) * Math.random() + a;
+  };
+
+  drawCircle = function(x, y, r, style) {
+    context.beginPath();
+    context.arc(x, y, r, 0, PI_2, false);
+    context.fillStyle = style;
+    return context.fill();
+  };
+
+  xpos = 0.5;
+
+  document.onmousemove = function(e) {
+    return xpos = e.pageX / w;
+  };
+
+  window.requestAnimationFrame = (function() {
+    return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(callback) {
+      return window.setTimeout(callback, 1000 / 60);
+    };
+  })();
+
+  Confetti = class Confetti {
+    constructor() {
+      this.style = COLORS[~~range(0, 5)];
+      this.rgb = `rgba(${this.style[0]},${this.style[1]},${this.style[2]}`;
+      this.r = ~~range(2, 6);
+      this.r2 = 2 * this.r;
+      this.replace();
+    }
+
+    replace() {
+      this.opacity = 0;
+      this.dop = 0.03 * range(1, 4);
+      this.x = range(-this.r2, w - this.r2);
+      this.y = range(-20, h - this.r2);
+      this.xmax = w - this.r;
+      this.ymax = h - this.r;
+      this.vx = range(0, 2) + 8 * xpos - 5;
+      return this.vy = 0.7 * this.r + range(-1, 1);
+    }
+
+    draw() {
+      var ref;
+      this.x += this.vx;
+      this.y += this.vy;
+      this.opacity += this.dop;
+      if (this.opacity > 1) {
+        this.opacity = 1;
+        this.dop *= -1;
+      }
+      if (this.opacity < 0 || this.y > this.ymax) {
+        this.replace();
+      }
+      if (!((0 < (ref = this.x) && ref < this.xmax))) {
+        this.x = (this.x + this.xmax) % this.xmax;
+      }
+      return drawCircle(~~this.x, ~~this.y, this.r, `${this.rgb},${this.opacity})`);
+    }
+
+  };
+
+  confetti = (function() {
+    var j, ref, results;
+    results = [];
+    for (i = j = 1, ref = NUM_CONFETTI; (1 <= ref ? j <= ref : j >= ref); i = 1 <= ref ? ++j : --j) {
+      results.push(new Confetti);
+    }
+    return results;
+  })();
+
+  window.step = function() {
+    var c, j, len, results;
+    requestAnimationFrame(step);
+    context.clearRect(0, 0, w, h);
+    results = [];
+    for (j = 0, len = confetti.length; j < len; j++) {
+      c = confetti[j];
+      results.push(c.draw());
+    }
+    return results;
+  };
+
+  step();
+
+}).call(this);
